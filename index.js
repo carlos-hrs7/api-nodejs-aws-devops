b98f39f211a9d750419098f2190d8bf3b8729331
Carlos Henrique <carlos.professionalti@gmail.com>
2025-07-17 20:22:15 -0300

    Alteracao mensagem do arquivo index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('A API está executando na Cloud AWS! Deploy automatizado com CodePipeline!');
+    res.send('Implantação de API simples em Cluster ECS na AWS e deploy automatizado com Code Pipeline (CI/CD)');
 });
 
 const port = process.env.PORT || 3000;
